f0fbd7dbb94dae738305780237f0e28bfa5a5533
priman <priman567@gmail.com>
2023-06-03 17:00:50 +0530

    initial commit

---
+++ back-end/src/main/java/lk/ijse/dep10/api/GalleryController.java
@@ -0,0 +1,23 @@
+package lk.ijse.dep10.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.Part;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/images")
+public class GalleryController {
+
+
+
+}
