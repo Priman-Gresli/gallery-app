a4c726ddac4597212790fe174e39c6be004f8c76
priman <priman567@gmail.com>
2023-06-13 08:37:31 +0530

    file upload done

--- front-end/dist/index.18dbc454.js
+++ front-end/dist/index.18dbc454.js
@@ -574,33 +574,70 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"1SICI":[function(require,module,exports) {
-const uploadEBtn = $("#btn");
-const uploadWindow = $("#outer-drag");
-const uploadArea = $("#drag-drop");
-const downloadElm = $(".material-symbols-outlined");
-const imageDivElm = $("#main-scene");
-const REST_API_URL = "http://localhost:8080/app";
-uploadEBtn.on("click", (event)=>{
-    uploadWindow.removeClass("d-none");
+const overlay = $("#overlay");
+const btnUpload = $("#btn-upload");
+const dropZoneElm = $("#drop-zone");
+const mainElm = $("main");
+const REST_API_URL = `http://localhost:8080/gallery`;
+const cssLoaderHtml = `<div class="lds-facebook"><div></div><div></div><div></div></div>`;
+loadAllImages();
+btnUpload.on("click", ()=>overlay.removeClass("d-none"));
+overlay.on("click", (evt)=>{
+    if (evt.target === overlay[0]) overlay.addClass("d-none");
 });
-uploadWindow.on("click", (event)=>{
-    if (event.target === uploadWindow[0]) uploadWindow.addClass("d-none");
+$(document).on("keydown", (evt)=>{
+    if (evt.key === "Escape" && !overlay.hasClass("d-none")) overlay.addClass("d-none");
 });
-$(document).on("keydown", (event)=>{
-    if (event.key === "Escape") uploadWindow.addClass("d-none");
+overlay.on("dragover", (evt)=>evt.preventDefault());
+overlay.on("drop", (evt)=>evt.preventDefault());
+dropZoneElm.on("dragover", (evt)=>{
+    evt.preventDefault();
 });
-downloadElm.on("click", (event)=>{});
-loadAll();
-function loadAll() {
-    const ajax = $.ajax(REST_API_URL + "/images");
-    ajax.done((imageList)=>{
-        imageList.forEach((imageUrl)=>{
-            console.log(imageUrl);
-            const divElm = $(`<div class="image"><span class="material-symbols-outlined">download</span></div>`);
-            divElm.css("background-image", `url(${imageUrl})`);
-            imageDivElm.append(divElm);
+dropZoneElm.on("drop", (evt)=>{
+    evt.preventDefault();
+    const droppedFiles = evt.originalEvent.dataTransfer.files;
+    const imageFiles = Array.from(droppedFiles).filter((file)=>file.type.startsWith("image/"));
+    if (!imageFiles.length) return;
+    overlay.addClass("d-none");
+    uploadImages(imageFiles);
+});
+mainElm.on("click", ".image:not(.loader)", (evt)=>{
+    evt.target.requestFullscreen();
+});
+function uploadImages(imageFiles) {
+    const formData = new FormData();
+    imageFiles.forEach((imageFile)=>{
+        const divElm = $(`<div class="image loader"></div>`);
+        divElm.append(cssLoaderHtml);
+        mainElm.append(divElm);
+        formData.append("images", imageFile);
+    });
+    const jqxhr = $.ajax(`${REST_API_URL}/images`, {
+        method: "POST",
+        data: formData,
+        contentType: false,
+        processData: false // by default jQuery tries to convert the data into String
+    });
+    jqxhr.done((imageUrlList)=>{
+        imageUrlList.forEach((imageUrl)=>{
+            const divElm = $(".image.loader").first();
+            divElm.css("background-image", `url('${imageUrl}')`);
+            divElm.empty();
+            divElm.removeClass("loader");
         });
     });
+    jqxhr.always(()=>$(".image.loader").remove());
+}
+function loadAllImages() {
+    const jqxhr = $.ajax(`${REST_API_URL}/images`);
+    jqxhr.done((imageUrlList)=>{
+        imageUrlList.forEach((imageUrl)=>{
+            const divElm = $(`<div class="image"></div>`);
+            divElm.css("background-image", `url(${imageUrl})`);
+            mainElm.append(divElm);
+        });
+    });
+    jqxhr.fail(()=>{});
 }
 
 },{}]},["kbGhK","1SICI"], "1SICI", "parcelRequire1f70")
--- front-end/src/js/main.js
+++ front-end/src/js/main.js
@@ -1,40 +1,76 @@
-const uploadEBtn = $("#btn");
-const uploadWindow = $("#outer-drag");
-const uploadArea = $("#drag-drop");
-const downloadElm = $(".material-symbols-outlined");
-const imageDivElm = $("#main-scene");
-const REST_API_URL="http://localhost:8080/app";
+const overlay = $("#overlay");
+const btnUpload = $("#btn-upload");
+const dropZoneElm = $("#drop-zone");
+const mainElm = $("main");
+const REST_API_URL = `http://localhost:8080/gallery`;
+const cssLoaderHtml = `<div class="lds-facebook"><div></div><div></div><div></div></div>`;
 
+loadAllImages();
 
-uploadEBtn.on('click',(event)=>{
-    uploadWindow.removeClass("d-none");
-
-})
-uploadWindow.on('click',(event)=>{
-    if (event.target === uploadWindow[0]) {
-       uploadWindow.addClass("d-none")
+btnUpload.on('click', () => overlay.removeClass('d-none'));
+overlay.on('click', (evt) => {
+    if (evt.target === overlay[0]) overlay.addClass('d-none');
+});
+$(document).on('keydown', (evt) => {
+    if (evt.key === 'Escape' && !overlay.hasClass('d-none')) {
+        overlay.addClass('d-none');
     }
-})
-$(document).on('keydown',(event=>{
-    if (event.key === "Escape") {
-        uploadWindow.addClass("d-none")
-    }
-}))
-downloadElm.on('click',(event)=>{
+});
+overlay.on('dragover', (evt) => evt.preventDefault());
+overlay.on('drop', (evt) => evt.preventDefault());
+dropZoneElm.on('dragover', (evt) => {
+    evt.preventDefault();
+});
+dropZoneElm.on('drop', (evt) => {
+    evt.preventDefault();
+    const droppedFiles = evt.originalEvent
+        .dataTransfer.files;
+    const imageFiles = Array.from(droppedFiles)
+        .filter(file => file.type.startsWith("image/"));
+    if (!imageFiles.length) return;
+    overlay.addClass("d-none");
+    uploadImages(imageFiles);
+});
+mainElm.on('click', '.image:not(.loader)', (evt)=> {
+    evt.target.requestFullscreen();
+});
 
-})
-loadAll();
+function uploadImages(imageFiles){
+    const formData = new FormData();
+    imageFiles.forEach(imageFile => {
+        const divElm = $(`<div class="image loader"></div>`);
+        divElm.append(cssLoaderHtml);
+        mainElm.append(divElm);
 
-function loadAll(){
-    const ajax = $.ajax(REST_API_URL+"/images");
-    ajax.done((imageList)=>{
-        imageList.forEach(imageUrl=>{
-            console.log(imageUrl);
-            const divElm = $(`<div class="image"><span class="material-symbols-outlined">download</span></div>`);
-            divElm.css('background-image', `url(${imageUrl})`);
-            imageDivElm.append(divElm);
-        })
-    })
+        formData.append("images", imageFile);
+    });
+    const jqxhr = $.ajax(`${REST_API_URL}/images`, {
+        method: 'POST',
+        data: formData,
+        contentType: false,         // by default jQuery uses application/x-www-form-urlencoded
+        processData: false          // by default jQuery tries to convert the data into String
+    });
+
+    jqxhr.done((imageUrlList)=> {
+        imageUrlList.forEach(imageUrl => {
+            const divElm = $(".image.loader").first();
+            divElm.css('background-image', `url('${imageUrl}')`);
+            divElm.empty();
+            divElm.removeClass('loader');
+        });
+    });
+    jqxhr.always(()=> $(".image.loader").remove());
 }
 
-
+function loadAllImages() {
+    const jqxhr = $.ajax(`${REST_API_URL}/images`);
+    jqxhr.done((imageUrlList) => {
+        imageUrlList.forEach(imageUrl => {
+            const divElm = $(`<div class="image"></div>`);
+            divElm.css('background-image', `url(${imageUrl})`);
+            mainElm.append(divElm);
+        });
+    });
+    jqxhr.fail(() => {
+    });
+}
